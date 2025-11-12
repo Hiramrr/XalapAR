854ce710f38baaf23d11fa9a001a4a895de8e15d
hiramrr <sasuke_77.23@hotmail.com>
2025-11-12 08:03:56 -0600

    configuracionnnn

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
+// svelte.config.js (apply over your provided file)
 import staticAdapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: staticAdapter(),
+		adapter: staticAdapter({
+			pages: 'dist',
+			assets: 'dist'
+		}),
 		paths: {
 			base: '/xalapAR'
 		}
