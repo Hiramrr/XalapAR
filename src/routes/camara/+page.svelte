534a1f1e619fa7c153ceb1973835d14daa426810
hiramrr <sasuke_77.23@hotmail.com>
2025-11-12 22:22:07 -0600

    se agrego un css que tal vez solucione lo de android

--- src/routes/camara/+page.svelte
+++ src/routes/camara/+page.svelte
@@ -1,28 +1,3 @@
-<!--
-<script>
-	const marker = document.querySelector('#markerLlama');
-	const scene = document.querySelector('a-scene');
-	const camera = document.querySelector('#camera');
-	let fixedModel = null;
-
-	marker.addEventListener('markerFound', () => {
-		if (fixedModel) return;
-
-		const tempModel = document.querySelector('#llamaTemp');
-		fixedModel = tempModel.cloneNode(true);
-		const markerPos = tempModel.object3D.getWorldPosition(new THREE.Vector3());
-		fixedModel.setAttribute('position', `${markerPos.x} ${markerPos.y} ${markerPos.z}`);
-		scene.appendChild(fixedModel);
-	});
-
-	document.addEventListener('keydown', (e) => {
-		const pos = camera.getAttribute('position');
-		if (e.key === 'q' || e.key === 'Q') pos.y += 0.1;
-		if (e.key === 'e' || e.key === 'E') pos.y -= 0.1;
-		camera.setAttribute('position', pos);
-	});
-</script>
--->
 <svelte:head>
 	<script src="https://aframe.io/releases/1.4.0/aframe.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/gh/AR-js-org/AR.js/aframe/build/aframe-ar.js"></script>
@@ -31,25 +6,37 @@
 	></script>
 </svelte:head>
 
-<main>
-	<div
-		style="margin: 0; overflow: hidden; position: fixed; top: 0; left: 0; width: 100%; height: 100%;"
-	>
-		<a-scene
-			embedded
-			arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;"
-			vr-mode-ui="enabled: false"
-		>
-			<a-marker type="pattern" url="/XalapAR/patt/sus.patt">
-				<a-entity
-					gltf-model="/XalapAR/modelos/llama.glb"
-					position="0 0 0"
-					rotation="0 100 0"
-					scale="0.01 0.01 0.01"
-					animation-mixer
-				/>
-			</a-marker>
-			<a-entity camera />
-		</a-scene>
-	</div>
-</main>
+<a-scene
+	embedded
+	arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;"
+	vr-mode-ui="enabled: false"
+>
+	<a-marker type="pattern" url="/XalapAR/patt/sus.patt">
+		<a-entity
+			gltf-model="/XalapAR/modelos/llama.glb"
+			position="0 0 0"
+			rotation="0 100 0"
+			scale="0.01 0.01 0.01"
+			animation-mixer
+		/>
+	</a-marker>
+	<a-entity camera />
+</a-scene>
+
+<style>
+	:global(body),
+	:global(html) {
+		margin: 0 !important;
+		overflow: hidden !important;
+		width: 100%;
+		height: 100%;
+	}
+
+	:global(a-scene) {
+		position: fixed !important;
+		top: 0;
+		left: 0;
+		width: 100% !important;
+		height: 100% !important;
+	}
+</style>
